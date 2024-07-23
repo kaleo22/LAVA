d1e853a36c5bade939ec1cb0ee88987fed17b0da
kaleo22 <leonard.kaempf@icloud.com>
2024-07-23 17:13:49 +0200

    bug fixing for establishing connection between Server, Containers and Laptop

--- docker/entrypoint_PoseTransform.sh
+++ docker/entrypoint_PoseTransform.sh
@@ -11,7 +11,7 @@ export ROS_DOMAIN_ID=10
 
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTRTPS_DEFAULT_PROFILES_FILE=/var/tmp/husarnet-fastdds-ds-client.xml
-export ROS_DISCOVERY_SERVER=1:0.0.0.0:11888
+export DISCOVERY_SERVER_PORT=11888
 husarnet-dds singleshot
 
 exec "$@"
