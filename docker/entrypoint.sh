5132e28135b88e0ed40368685477ece3b11eae0e
kaleo22 <leonard.kaempf@icloud.com>
2024-07-23 16:46:47 +0200

    added setup in entrypoints for FastDDS Discovery Server

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,4 +9,9 @@ source /ros_ws/peak_cam/install/setup.bash
 export ROS_DOMAIN_ID=10
 
 
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export FASTRTPS_DEFAULT_PROFILES_FILE=/var/tmp/husarnet-fastdds-ds-client.xml
+export ROS_DISCOVERY_SERVER=1:0.0.0.0:11888
+husarnet-dds singleshot
+
 exec "$@"
